43fef9f7c42d1fdf53ef399796ba9b42ae67832c
99002597 <gaurav.das@ltts.com>
2020-11-05 15:20:12 +0530

    Added HomeController & edited Views

---
+++ Login/Models/DB_Entities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Login.Models
+{
+    public class DB_Entities : DbContext
+    {
+        public DB_Entities() : base("Db_website") { }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<demoEntities>(null);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+
+}
